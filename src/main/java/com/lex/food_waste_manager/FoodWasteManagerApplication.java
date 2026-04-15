25fa4bf83aecc3e6669a9f81a8d6fa85011c73ef
LexSnow <108388996+LexSnow@users.noreply.github.com>
2026-04-15 15:42:08 +0200

    Zainicjalizowano Spring

---
+++ src/main/java/com/lex/food_waste_manager/FoodWasteManagerApplication.java
@@ -0,0 +1,13 @@
+package com.lex.food_waste_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodWasteManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodWasteManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lex/food_waste_manager/FoodWasteManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lex.food_waste_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodWasteManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
